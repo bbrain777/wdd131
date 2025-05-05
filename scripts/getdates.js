f230d29e08f983b65b41db9e26ced155d052f289
Your Name <your-email@example.com>
2025-05-05 16:10:53 +0100

    neccesary adjustment made

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,8 @@
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent =
+        "Last Modified: " + lastModified;
+});
